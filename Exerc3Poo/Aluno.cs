d80472e58902baea25d10fe224fac137b3d020b4
tlima1011 <thiagolima1011@gmail.com>
2026-01-18 21:40:33 -0300

    Projeto Criado Exerc3Poo - C#

---
+++ Exerc3Poo/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc3Poo
+{
+    internal class Aluno
+    {
+        public string nome; 
+        public double nota1;
+        public double nota2;
+        public double nota3; 
+
+        public double somar()
+        {
+            return nota1 + nota2 + nota3;  
+        }
+
+        public override string ToString()
+        {
+            double restante = 60.00 - somar(); 
+                    
+            Console.WriteLine($"NOTA FINAL = {somar().ToString("F2", CultureInfo.InvariantCulture)}");
+            return somar() >= 60.00 ? "APROVADO" : $"REPROVADO\nFALTARAM {restante.ToString("F2", CultureInfo.InvariantCulture)} PONTOS";     
+        }
+    }
+}
---
+++ Exerc3Poo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization; 
+
+namespace Exerc3Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            a.nome = Console.ReadLine();
+            Console.WriteLine($"Digite as três notas do aluno(a): {a.nome}");
+            a.nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            a.nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            a.nota3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine(a);
+        }
+    }
+}
